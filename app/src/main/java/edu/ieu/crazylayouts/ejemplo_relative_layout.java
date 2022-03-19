72d568e3a8151ff9c634582d704a79b76b2fdf76
MicheelG1 <micheelgarcia870@gmail.com>
2022-03-18 20:39:06 -0600

    Initial commit

---
+++ app/src/main/java/edu/ieu/crazylayouts/ejemplo_relative_layout.java
@@ -0,0 +1,14 @@
+package edu.ieu.crazylayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ejemplo_relative_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo_relative_layout);
+    }
+}
